e665d87bdfcafeb1f29a0b2b55633c02e53b88cd
unyss98 <eunicedunyo@gmail.com>
2023-04-28 16:04:03 +0000

    debugging for null pointer s specifier

--- printf.c
+++ printf.c
@@ -33,7 +33,7 @@ int _printf(const char *format, ...)
 				case 's':
 					sv = va_arg(list, char*);
 					if (sv == NULL)
-						fputs("(null", stdout), count += 5;
+						fputs("(null)", stdout), count += 5;
 					for (j = 0; sv[j] != '\0'; j++)
 						putchar(sv[j]), count++;
 					break;
